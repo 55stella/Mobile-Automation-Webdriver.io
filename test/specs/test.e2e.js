81f692b8da76c1181fbc86aa8d7382262e976c5b
55stella <stellarichards7@gmail.com>
2024-04-17 14:25:35 +0100

    initial commit

---
+++ test/specs/test.e2e.js
@@ -0,0 +1,29 @@
+// import { expect } from '@wdio/globals'
+// import LoginPage from '../pageobjects/login.page.js'
+// import SecurePage from '../pageobjects/secure.page.js'
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        // await LoginPage.open()
+       console.log("test 1")
+        // await LoginPage.login('tomsmith', 'SuperSecretPassword!')
+        // await expect(SecurePage.flashAlert).toBeExisting()
+        // await expect(SecurePage.flashAlert).toHaveTextContaining(
+        //     'You logged into a secure area!')
+    })
+
+
+    it("test 2", async () => {
+        // await LoginPage.open();
+        console.log("test 2")
+
+    //   await LoginPage.login("tomsmith", "SuperSecretPassword!");
+    //   await expect(SecurePage.flashAlert).toBeExisting();
+    //   await expect(SecurePage.flashAlert).toHaveTextContaining(
+    //     "You logged into a secure area!"
+    //   );
+    });
+
+
+})
+
